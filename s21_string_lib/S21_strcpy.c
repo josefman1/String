b4a8e98792fc0a54cbca04f4f59474d876a9685c
lezod <lezod@Linux.myguest.virtualbox.org>
2024-02-13 14:23:45 +0300

    Kod est', ostalis' testi

--- s21_string_lib/S21_strcpy.c
+++ s21_string_lib/S21_strcpy.c
@@ -1,1 +1,11 @@
+#include "s21_string.h"
 
+char *s21_strcpy(char *destination, const char *src) {
+    for (int i = 0; src[i]; i++) {
+        destination[i] = src[i];
+    }
+
+    destination[s21_strlen(src)] = '\0';
+
+    return destination;
+}
--- s21_string_lib/S21_string.h
+++ s21_string_lib/S21_string.h
@@ -1,2 +1,9 @@
 #include "main.c"
 #define s21_NULL ((void *)0);
+typedef long unsigned s21_size_t;
+
+char *s21_strchr(const char *str, int c);
+char *s21_strcpy(char *destination, const char *src);
+s21_size_t s21_strlen(const char *str);
+char *s21_strstr(const char *destptr, const char *srcptr);
+char* strtok(char* str, const char* delim);
